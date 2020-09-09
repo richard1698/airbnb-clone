234e28b92647c9c996ec5905dde9433cf82af91c
Richard Garcia <rjgman@pacifico.fin.ec>
2020-09-08 23:53:35 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Airbnb clone</h1>
+    <div className="app">
+      <Header />
+      <Home />
+      {/*Home*/}
+      {/* Header */}
+      {/* Banner */}
+      {/*Search*/}
+      {/*Cards*/}
+      {/*Footer*/}
+      {/*SearchPage*/}
+      {/* ... */}
     </div>
   );
 }
