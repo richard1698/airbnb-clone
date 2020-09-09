1e42cd64a729da4ee243f1d3058b2d9aa84f456c
Richard Garcia <rjgman@pacifico.fin.ec>
2020-09-09 12:08:08 -0500

    Date picker Component

---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Search.css";
+
+//Date picker component
+function Search() {
+  return (
+    <div className="search">
+      <h1>Cola</h1>
+    </div>
+  );
+}
+
+export default Search;
